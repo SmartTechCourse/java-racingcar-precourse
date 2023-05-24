066c79583c4f2c7dc4c78604326398a682dfd4c7
박진영 <tear4321@cau.ac.kr>
2023-05-25 01:40:11 +0900

    refactor/gameoutput code format

--- src/main/java/racingcar/controller/io/GameOutput.java
+++ src/main/java/racingcar/controller/io/GameOutput.java
@@ -5,11 +5,16 @@ import racingcar.domain.RaceTrack;
 import java.util.List;
 
 public interface GameOutput {
-    public void showCarInputIndicator();
-    public void showCountInputIndicator();
-    public void showResult();
 
-    public void showRaceTrackState(RaceTrack raceTrack);
-    public void showWinner(List<String> winners);
-    public void showError(String errorMessage);
+    void showCarInputIndicator();
+
+    void showCountInputIndicator();
+
+    void showResult();
+
+    void showRaceTrackState(RaceTrack raceTrack);
+
+    void showWinner(List<String> winners);
+
+    void showError(String errorMessage);
 }
