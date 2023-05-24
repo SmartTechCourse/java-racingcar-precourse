faa9c1bd28075440a234a01a752eee36f21a2c2d
박진영 <tear4321@cau.ac.kr>
2023-05-24 23:40:38 +0900

    feature/track의 상태추가(goalin 했는지 안했는지)

--- src/main/java/racingcar/domain/RaceTrack.java
+++ src/main/java/racingcar/domain/RaceTrack.java
@@ -4,12 +4,18 @@ import java.util.List;
 
 public class RaceTrack {
     private final List<Car> cars;
+    private final int trackSize;
 
-    public RaceTrack(List<Car> cars) {
+    public RaceTrack(List<Car> cars, int trackSize) {
         this.cars = cars;
+        this.trackSize = trackSize;
     }
 
     public List<Car> getCars() {
         return cars;
+    }
+
+    public boolean isGoalInAtLeastOne() {
+        return cars.stream().anyMatch(car -> car.getPosition() >= trackSize);
     }
 }
